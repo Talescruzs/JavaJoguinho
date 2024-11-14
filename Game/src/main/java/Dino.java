db7f42830e52b8e14155fa6f39e111a5bacdd846
Talescruzs <tcs2017@hotmail.com>
2024-11-14 08:09:33 -0300

    add mini inicio

---
+++ Game/src/main/java/Dino.java
@@ -0,0 +1,12 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+
+public class Dino {
+    public Dino(int id){
+        
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JavaJoguinho
+
+Instalar maven (sudo apt install maven)
+
+Intalar dependencias (mvn install)
